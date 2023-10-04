9c13dd6dea00e366c0169ef305b2fde2f0422945
Adrián <adrsabper@gmail.com>
2023-10-04 21:33:06 +0200

    .

--- app/src/main/java/com/PMDM/actividad01pmdm/MainActivity.kt
+++ app/src/main/java/com/PMDM/actividad01pmdm/MainActivity.kt
@@ -2,7 +2,7 @@ package com.PMDM.actividad01pmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,3 +11,17 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+class FirstActivity : AppCompatActivity() {
+    var mainButton = findViewById<Button>(R.id.button1)
+    fun initlistener(){
+
+    }
+    private fun clickCount(){
+        var contador:Int=0
+        while (contador < 5){
+            when (contador){
+                1 ->{}
+            }
+        }
+    }
+}
